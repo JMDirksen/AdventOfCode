638f6b9abe604339aa063caffb8054339dd3f97f
JMDirksen <jeftadirksen@gmail.com>
2024-12-11 09:06:16 +0100

    string -> int[]

--- 2024/9/script.py
+++ 2024/9/script.py
@@ -2,7 +2,7 @@ import re
 
 
 def main():
-    inputfile = "example.txt"  # example.txt / input.txt
+    inputfile = "input.txt"  # example.txt / input.txt
     with open(inputfile) as f:
         input = f.read()
     print(part1(input))
@@ -11,41 +11,44 @@ def main():
 def part1(input):
 
     # Uncompress
+    print("Uncompressing...")
     file = True
     id = 0
-    disk_map = ""
+    disk_map = []
     for i in input:
         if file:
             for x in range(int(i)):
-                disk_map += str(id)
+                disk_map += [id]
             id += 1
         else:
             for x in range(int(i)):
-                disk_map += "."
+                disk_map += [None]
         file = not file
-    print("Uncompressed:", disk_map)
 
     # Defragment
+    print("Defragmenting...")
     while True:
-        # First digit from right
-        block_r = len(disk_map)-1-re.search("\d", disk_map[::-1]).start()
+        # First block with data from right
+        for br in range(len(disk_map)-1, -1, -1):
+            if disk_map[br] != None:
+                break
         # First free space from left
-        search_space = re.search("\.", disk_map[0:block_r+1])
-        if not search_space:
+        for bl in range(0, br):
+            if disk_map[bl] == None:
+                break
+        if bl == br-1:
             break
-        space_l = search_space.start()
         # Move block
-        disk_map = list(disk_map)
-        disk_map[space_l] = disk_map[block_r]
-        disk_map[block_r] = "."
-        disk_map = "".join(disk_map)
-    
+        disk_map[bl] = disk_map[br]
+        disk_map[br] = None
+
     # Calculate checksum
-    files_map = disk_map.replace(".", "")
+    print("Calculating checksum...")
     checksum = 0
-    for pos in range(len(files_map)):
-        checksum += pos * int(files_map[pos])
-    
+    for i in range(len(disk_map)):
+        if disk_map[i] == None:
+            break
+        checksum += i * disk_map[i]
     return checksum
 
 
