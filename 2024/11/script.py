c798a6a0c6d5dd92b38d93e4a0e94372a09a99a9
JMDirksen <jeftadirksen@gmail.com>
2024-12-12 20:53:26 +0100

    counted values

--- 2024/11/script.py
+++ 2024/11/script.py
@@ -7,7 +7,7 @@ def main():
     inputfile = "input.txt"  # example.txt / input.txt
     with open(inputfile) as f:
         input = f.read()
-    print(part1(input))
+    print(part2(input))
 
 
 def part1(input):
@@ -32,7 +32,43 @@ def part1(input):
 
 
 def part2(input):
-    return
+    start = time()
+    stones = list(map(int, input.split()))
 
+    # List with nr counts
+    for s, stone in enumerate(stones):
+        stones[s] = [stone, 1]
+
+    # Blinks
+    for b in range(75):
+        print(f"Blink {b+1} at {int(time()-start)} sec.")
+        output = []
+        for s, stone in enumerate(stones):
+            # Apply rules
+            if stone[0] == 0:
+                add_value(output, 1, stone[1])
+            elif len(str(stone[0])) % 2 == 0:
+                add_value(output, int(str(stone[0])[:len(str(stone[0]))//2]), stone[1])
+                add_value(output, int(str(stone[0])[len(str(stone[0]))//2:]), stone[1])
+            else:
+                add_value(output, stone[0]*2024, stone[1])
+        stones = output
+
+    count = 0
+    for stone in stones:
+        count += stone[1]
+    return count
+
+
+def add_value(stonesList, value, count):
+    found = False
+    for s, stone in enumerate(stonesList):
+        if stone[0] == value:
+            stonesList[s][1] += count
+            found = True
+            break
+    if not found:
+        stonesList.append([value, count])
+        
 
 main()
