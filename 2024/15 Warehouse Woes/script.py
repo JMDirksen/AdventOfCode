7d82fc077c42afd956e816856beaddcbc6e26e7a
JMDirksen <jeftadirksen@gmail.com>
2024-12-15 15:00:29 +0100

    15 p2

--- 2024/15 Warehouse Woes/script.py
+++ 2024/15 Warehouse Woes/script.py
@@ -5,7 +5,7 @@ def main():
     inputfile = "input.txt"  # example.txt / input.txt
     with open(inputfile) as f:
         input = f.read()
-    print(part1(input))
+    print(part2(input))
 
 
 def part1(input):
@@ -25,6 +25,66 @@ def part1(input):
             elif column == "O":
                 box = Box(room, Position(c, r))
                 room.boxes.append(box)
+                room.grid[r][c] = box
+            elif column == "@":
+                robot = Robot(room, Position(c, r))
+                room.robots.append(robot)
+                room.grid[r][c] = robot
+
+    # Movements
+    movements = ""
+    for line in input.splitlines():
+        if line.startswith("#"):
+            continue
+        movements += line
+    movements = list(map(Movement, list(movements)))
+
+    # Iterate movements
+    room.display("0%")
+    for m, move in enumerate(movements):
+        robot.move(move)
+        if m % 100 == 0:
+            percent = int((m+1)/len(movements)*100)
+            title = f"{percent}%"
+            room.display(title, redraw=True, dwell=0)
+
+    # GPS sum
+    gps_sum = 0
+    for box in room.boxes:
+        gps_sum += box.pos.gps()
+
+    return gps_sum
+
+
+def part2(input):
+    # Room
+    grid = []
+    for line in input.splitlines():
+        if not line.startswith("#"):
+            break
+        grid_line = []
+        for c in list(line):
+            if c == "#":
+                grid_line.extend(["#", "#"])
+            elif c == "O":
+                grid_line.extend(["[", "]"])
+            elif c == ".":
+                grid_line.extend([".", "."])
+            elif c == "@":
+                grid_line.extend(["@", "."])
+        grid.append(grid_line)
+    room = Room(len(grid[0]), len(grid), "Warehouse2")
+
+    # Fill room
+    for r, row in enumerate(grid):
+        for c, column in enumerate(row):
+            if column == "#":
+                room.grid[r][c] = "#"
+            elif column == "[":
+                box = Box(room, Position(c, r), 2)
+                room.boxes.append(box)
+                room.grid[r][c] = box
+            elif column == "]":
                 room.grid[r][c] = box
             elif column == "@":
                 robot = Robot(room, Position(c, r))
@@ -92,16 +152,21 @@ class Room:
         if redraw:
             print("\033[F"*(len(self.grid)+2))
         print(title)
+        box_char = "["
         for row in self.grid:
             for space in row:
                 if isinstance(space, Robot):
-                    char = "@ "
+                    char = "@"
                 elif isinstance(space, Box):
-                    char = "O "
+                    char = box_char
+                    if box_char == "[":
+                        box_char = "]"
+                    else:
+                        box_char = "["
                 elif space == "#":
-                    char = "# "
+                    char = "#"
                 else:
-                    char = ". "
+                    char = "."
                 print(char, end="")
             print()
         sleep(dwell)
@@ -153,37 +218,69 @@ class Robot:
 
 
 class Box:
-    def __init__(self, room: Room, pos: Position):
+    def __init__(self, room: Room, pos: Position, width=1):
         self.room = room
         self.pos = pos
+        self.width = width
 
     def __str__(self):
         return f"Box({self.room}, {self.pos})"
 
     def push(self, move: Movement) -> bool:
-        new_pos = Position(self.pos.x + move.x, self.pos.y + move.y)
-        at_pos = self.room.look(new_pos)
-        if at_pos == None:
-            self.move_to(new_pos)
+        new_pos_list = []
+        at_pos_list = []
+        for w in range(self.width):
+            new_pos_list.append(
+                Position(self.pos.x + move.x + w, self.pos.y + move.y))
+            at_pos_list.append(self.room.look(new_pos_list[w]))
+
+        if all(at_pos == None for at_pos in at_pos_list):
+            self.move_to(new_pos_list[0])
             return True
-        elif isinstance(at_pos, Box):
-            if at_pos.push(move):
-                self.move_to(new_pos)
-                return True
-            else:
-                return False
+        elif any(at_pos == "#" for at_pos in at_pos_list):
+            return False
+
+        can_move = True
+        for at_pos in at_pos_list:
+            if isinstance(at_pos, Box) and at_pos != self and not at_pos.can_push(move):
+                can_move = False
+                break
+        if can_move:
+            for pos in new_pos_list:
+                if isinstance(self.room.look(pos), Box) and self.room.look(pos) != self:
+                    self.room.look(pos).push(move)
+            self.move_to(new_pos_list[0])
+            return True
         else:
             return False
+
+    def can_push(self, move: Movement) -> bool:
+        new_pos_list = []
+        at_pos_list = []
+        for w in range(self.width):
+            new_pos_list.append(
+                Position(self.pos.x + move.x + w, self.pos.y + move.y))
+            at_pos_list.append(self.room.look(new_pos_list[w]))
+
+        if all(at_pos == None for at_pos in at_pos_list):
+            return True
+
+        if any(at_pos == "#" for at_pos in at_pos_list):
+            return False
+
+        for at_pos in at_pos_list:
+            if isinstance(at_pos, Box) and at_pos != self and not at_pos.can_push(move):
+                return False
+
+        return True
 
     def move_to(self, new_pos: Position):
         old_pos = self.pos
         self.pos = new_pos
-        self.room.grid[old_pos.y][old_pos.x] = None
-        self.room.grid[new_pos.y][new_pos.x] = self
-
-
-def part2(input):
-    return
+        for w in range(self.width):
+            self.room.grid[old_pos.y][old_pos.x + w] = None
+        for w in range(self.width):
+            self.room.grid[new_pos.y][new_pos.x + w] = self
 
 
 main()
